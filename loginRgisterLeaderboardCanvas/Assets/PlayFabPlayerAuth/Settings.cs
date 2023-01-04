938ddc202054d19f51a074925357d329cfa18271
SopapillaKat <vellita2011@gmail.com>
2023-01-04 11:02:49 -0600

    dsg

---
+++ loginRgisterLeaderboardCanvas/Assets/PlayFabPlayerAuth/Settings.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+   public static bool GameIsPaused = false;
+
+    public GameObject settingsMenuUI;
+    public Button backButton;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        settingsMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        //bullets.SetActive(true);
+    }
+
+    void Pause()
+    {
+        settingsMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        //bullets.SetActive(false);
+    }
+
+    public void OnBackButton()
+    {
+        settingsMenuUI.SetActive(false);
+    }
+
+}
